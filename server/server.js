19ba4c7d2bbd4915dcd495962c4529817f85b753
serge shalo wilson <sergeshalowilson@gmail.com>
2017-11-02 19:48:53 -0700

    get todos id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 const bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 
 var {mongoose} = require('./db/mongoose');
@@ -21,6 +22,39 @@ app.post('/todos', (req, res) => {
    });
 });
 
+
+
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos})
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+
+});
+
+
 app.listen(3000, () => {
   console.log("started on port 3000");
 });
+
+
+module.exports = {app};
